71d34142ba34c56be1276f8720bf0561e2e625f4
KyokoSamura <samura.kyoko@gmail.com>
2021-03-14 18:45:05 +0900

    jQuery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+$("#hamburger").click(function () {
+    $(this).toggleClass("active");
+    $(".menu-content").toggleClass("open");
+});
+
 $('#top-btn').click(function () {
   $('html,body').animate({
     scrollTop: 0
